9ac43f3f83d0f1f611d3019af17f2b7f344964c7
Matan Nassau <matan.nassau@gmail.com>
2015-08-03 23:14:17 -0400

    reserve{0} vector has capacity >= 0

--- test/reserve.cc
+++ test/reserve.cc
@@ -7,3 +7,9 @@ TEST_CASE("reserve{5} vector has capacity >= 5") {
   auto const a = std::vector<int>(rsv::reserve{5}).capacity();
   REQUIRE(x <= a);
 }
+
+TEST_CASE("reserve{0} vector has capacity >= 0") {
+  auto const x = 0;
+  auto const a = std::vector<int>(rsv::reserve{0}).capacity();
+  REQUIRE(x <= a);
+}
